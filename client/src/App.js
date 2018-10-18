c984fb17fdb474215259af3e99deb6766b9d11ec
Olesia <olesya.kovalchuk13@gmail.com>
2018-10-17 18:43:34 -0600

    index.js changed and clientManager fila added

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,44 @@ class App extends Component {
 		this.state = {
 			socket: openSocket( 'http://localhost:5000' ),
 			message: 'Waiting for another player...',
-			yourTurn: false,
+			yourTurn: false
 		}
 
 	}
-	click = ( e ) => {
-		this
-			.state
-			.socket
-			.emit( 'click', e.currentTarget.id )
-	}
+	// if ( e.currentTarget.className === 'deck' ) {
+	// 	click = ( e, this.state.hand ) => {
+	// 		this
+	// 			.state
+	// 			.socket
+	// 			.emit( 'click', e.currentTarget.id, )
+	// 	}
+	// }
+	// click = ( e, ) => {
+	// 	if ( e.currentTarget.id === 'deck' ) {
+	// 	 need to pass hand
+	// 		this
+	// 			.state
+	// 			.socket
+	// 			.emit( 'click', e.currentTarget.id, this.state.hand )
+	// 	} else if ( e.currentTarget.id = 'hand' ) {
+	// 	 need to pass deck
+	// 		this
+	// 			.state
+	// 			.socket
+	// 			.emit( 'click', e.currentTarget.id, this.state.deck )
+	// 	}
+	// 	this
+	// 		.state
+	// 		.socket
+	// 		.emit( 'click', e.currentTarget.className )
+	// }
 
 	render() {
 		return ( <div className="App">
 			<h1 className="tag" onClick={this.click}>Hi</h1>
+			<div className="deck" onClick={this.click}>
+				<p></p>
+			</div>
 		</div> );
 	}
 }
---
+++ config/users.js
@@ -0,0 +1,11 @@
+module.exports = [
+	{
+		name: 'Rick'
+	}, {
+		name: 'Daryl'
+	}, {
+		name: 'Carol'
+	}, {
+		name: 'Megan'
+	},
+]
