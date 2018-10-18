0a3045571b9d57240ccc120406ddd1a63cb5478d
Bryce Kilker <brycekilker@gmail.com>
2018-10-18 11:39:56 -0600

    Created function to draw card from deck to hand at start of game. Began implementing count.

--- client/src/components/GameCard/GameCard.js
+++ client/src/components/GameCard/GameCard.js
@@ -10,13 +10,48 @@ class GameCard extends Component {
 		// 	.emit( 'click', e.currentTarget.className )
 	}
 	render() {
-		const { classes } = this.props;
-		if ( this.props.className === "player_deck" || this.props.className === "opponents_deck" || this.props.className === "player_stack" || this.props.className === "opponents_stack" ) 
-			return ( <CardMedia component="img" alt={this.props.className} className={`${ this.props.className} ${ classes.card }`} onClick={this.clickHandler} image={require( `../../images/card_back.png` )}/> )
-		else if ( this.props.className === "opponents_discard" || this.props.className === "player_discard" ) 
-			return ( <CardMedia component="img" alt={this.props.className} className={`${ this.props.className} ${ classes.card }`} onClick={this.clickHandler} image={require( `../../images/discard.png` )}/> )
-		else 
-			return ( <CardMedia component="img" alt={this.props.className} className={`${ this.props.className} ${ classes.card }`} onClick={this.clickHandler} image={require( `../../images/${ this.props.className }.png` )}/> )
+		const { classes, count } = this.props;
+		const deckOrStack = this.props.className === "player_deck" || this.props.className === "opponents_deck" || this.props.className === "player_stack" || this.props.className === "opponents_stack"
+		let card;
+		if ( deckOrStack )
+			card = (
+				<CardMedia
+					component="img"
+					alt={this.props.className}
+					className={`${ this.props.className} ${ classes.card }`}
+					onClick={this.clickHandler}
+					image={require( `../../images/card_back.png` )}
+				/>
+			)
+		else if ( this.props.className === "opponents_discard" || this.props.className === "player_discard" )
+			card = (
+				<CardMedia
+					component="img"
+					alt={this.props.className}
+					className={`${ this.props.className} ${ classes.card }`}
+					onClick={this.clickHandler}
+					image={require( `../../images/discard.png` )}
+				/>
+			)
+		else
+			card = (
+				<CardMedia
+					component="img"
+					alt={this.props.className}
+					className={`${ this.props.className} ${ classes.card }`}
+					onClick={this.clickHandler}
+					image={require( `../../images/${ this.props.className }.png` )}
+				/>
+		)
+		if (!!count) {
+			card = (
+				<div>
+					{card}
+					<p className="counter">{count}</p>
+				</div>
+			)
+		}
+		return card;
 
 	}
 }
