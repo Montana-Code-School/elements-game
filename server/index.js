1f9743f5981b30472b6220604164437d8a5d91db
Olesia <olesya.kovalchuk13@gmail.com>
2018-10-18 21:20:23 -0600

    Server keeps track of all clients and all rooms

--- server/index.js
+++ server/index.js
@@ -3,57 +3,41 @@ const app = express();
 const port = process.env.PORT || 5000;
 const server = require( 'http' ).createServer()
 const io = require( 'socket.io' )( server )
-
+const _ = require( "underscore" );
 const ClientManager = require( './ClientManager' )
 
-let counter = 0;
-const players = {
-	player1: null,
-	player2: null,
-}
-let player = 'player1';
-
-function reset() {
-	players[ 'player1' ] = null
-	players[ 'player2' ] = null
-	player = 'player1'
-}
 const clientManager = ClientManager()
-
+let counter = 0;
 io.on( 'connection', function ( client ) {
 	console.log( 'client connected...', client.id )
 	clientManager.addClient( client )
 
 	client.on( 'join', async function () {
 
+		const { rooms } = io.sockets.adapter;
+
 		for ( let i = 0; i <= counter; i++ ) {
 			if ( !!io.sockets.adapter.rooms[ `room${ i }` ] ) {
 				if ( io.sockets.adapter.rooms[ `room${ i }` ].length === 1 ) {
-					await new Promise( ( resolve ) => {
-						client.join( `room${ i }`, function () {
-							resolve();
-						} );
-					} )
-				} else {
+					client.join( `room${ i }` );
+					console.log( 'client joined: ', `room${ i }` );
+					break;
+				} else if ( io.sockets.adapter.rooms[ `room${ i }` ].length === 2 && i === counter ) {
 					counter++;
 				}
 			} else {
-				await new Promise( ( resolve ) => {
-					client.join( `room${ i }`, function () {
-						resolve();
-					} );
-				} )
+				client.join( `room${ i }` );
 				console.log( 'new room created client joined: ', `room${ counter }` );
+				break;
 			}
 		}
 		console.log( "all rooms", io.sockets.adapter.rooms )
 	} )
-	// console.log( "rooms", playing_room ), });
 	client.on( 'disconnect', function () {
 		console.log( 'client disconnect...', client.id );
 		// remove user
 		clientManager.removeClient( client )
-		console.log( io.sockets.adapter.rooms )
+		console.log( "all rooms", io.sockets.adapter.rooms )
 	} );
 	client.on( 'error', function ( err ) {
 		console.log( 'received error from client:', client.id )
