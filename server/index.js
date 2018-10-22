2932a691a2c04bf64f740a733b9570e77b541aa4
Bryce Kilker <brycekilker@gmail.com>
2018-10-22 11:34:49 -0600

    added express to server index

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const connect = require( 'connect' );
 const serveStatic = require('serve-static');
+const express = require('express')
+const app = express()
 const path = require('path')
 const port = process.env.PORT || 5000;
-const server = require( 'http' ).createServer();
+const server = require('http').createServer(app);
 const io = require( 'socket.io' )( server );
 const _ = require( "underscore" );
 const ClientManager = require( './ClientManager' );
@@ -91,9 +93,16 @@ io.on( 'connection', function ( client ) {
 		console.log( err )
 	} )
 } );
-if (process.env.NODE_ENV === 'production') {
-	connect().use(serveStatic(path.join(__dirname, 'client/build')))
-}
+// if (process.env.NODE_ENV === 'production') {
+	// app.use(express.static(__dirname + '/client/build'))
+	// connect().use(serveStatic( './client/build/index.html'))
+// }
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 server.listen( port, function ( err ) {
 	if ( err )
 		throw err
