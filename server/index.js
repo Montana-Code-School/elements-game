f8fd75c9501b608034dc60c7a52e8c8e0e789501
Olesia <olesya.kovalchuk13@gmail.com>
2018-10-26 14:23:39 -0600

    icon deleted

--- server/index.js
+++ server/index.js
@@ -112,7 +112,7 @@ io.on( "connection", function ( client ) {
 	} )
 } );
 if ( process.env.NODE_ENV === "production" ) {
-	app.use( express.static( path.join( __dirname, "../client / build " ) ) );
+	app.use( express.static( path.join( __dirname, "../client/build" ) ) );
 	app.get( "/", function ( req, res ) {
 		res.sendFile( path.join( __dirname, "../client/build", "index.html" ) );
 	} );
