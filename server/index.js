a701146fa763123e238c2c6c1cce4ab23d2fdcc1
Olesia <olesya.kovalchuk13@gmail.com>
2018-10-26 15:17:10 -0600

    try to fix error

--- server/index.js
+++ server/index.js
@@ -112,6 +112,7 @@ io.on( "connection", function ( client ) {
 	} )
 } );
 if ( process.env.NODE_ENV === "production" ) {
+	console.log( "Im here" );
 	app.use( express.static( path.join( __dirname, "../client/build" ) ) );
 	app.get( "/", function ( req, res ) {
 		res.sendFile( path.join( __dirname, "../client/build", "index.html" ) );
