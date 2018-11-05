d6f47f21ca188ade65e8fc0b15469891bb4c5838
Bryce Kilker <brycekilker@gmail.com>
2018-11-05 13:05:18 -0700

    added new components for CardCount and refactored state

--- client/src/components/DiscardPile.js
+++ client/src/components/DiscardPile.js
@@ -1,49 +1,33 @@
-import React, { Component } from "react";
-import { CardMedia, withStyles, } from "@material-ui/core";
+import React, {Component} from "react";
+import {CardMedia, withStyles} from "@material-ui/core";
 import CustomModal from "./Modal";
-import { Card as styles } from "./AllStyles";
+import {Card as styles} from "./AllStyles";
 import CardDisplay from "./CardDisplay";
-import { Grid } from "@material-ui/core";
+import {Grid} from "@material-ui/core";
+import CardCount from "./CardCount"
 
 class DiscardPile extends Component {
-	state = {
-		showModal: false
-	};
-	close = () => {
-		this.setState( { showModal: false } );
-	};
+  state = {
+    showModal: false
+  };
+  close = () => {
+    this.setState({showModal: false});
+  };
 
-	open = () => {
-		this.setState( { showModal: true } )
-	};
-	render() {
-		const { classes, cards, } = this.props;
-		return ( <div className={classes.card}>
-			<CardMedia
-				onClick={this.open}
-				component="img"
-				alt={this.props.className}
-				className={this.props.className}
-				image={require( `../images/discard.png` )}/>
-			<CustomModal
-				isOpen={this.state.showModal}
-				closeModal={this.close}>
-				<CardDisplay onClick={this.props.onClick}/>
-				<Grid
-					container={true}
-					direction="row"
-					justify="space-around"
-					alignItems="center">
-					<p>{cards[ "water" ]}</p>
-					<p>{cards[ "earth" ]}</p>
-					<p>{cards[ "light" ]}</p>
-					<p>{cards[ "shadow" ]}</p>
-					<p>{cards[ "fire" ]}</p>
-				</Grid>
-			</CustomModal>
-		</div> );
-	}
+  open = () => {
+    this.setState({showModal: true})
+  };
+  render() {
+    const {classes} = this.props;
+    return (<div className={classes.card}>
+      <CardMedia onClick={this.open} component="img" alt={this.props.className} className={this.props.className} image={require(`../images/discard.png`)}/>
+      <CustomModal isOpen={this.state.showModal} closeModal={this.close}>
+        <CardDisplay onClick={this.props.onClick}/>
+        <CardCount cards={this.props.cards}/>
+      </CustomModal>
+    </div>);
+  }
 
 }
 
-export default withStyles( styles )( DiscardPile )
+export default withStyles(styles)(DiscardPile)
