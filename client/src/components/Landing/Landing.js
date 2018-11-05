0170f7f49fef44fa5ee5bca37bb165effded9145
Olesia <olesya.kovalchuk13@gmail.com>
2018-11-04 21:25:33 -0700

    merged with updates

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -6,10 +6,12 @@ class Landing extends Component {
 	render() {
 		return ( <div className="landingContainer" id="landingPage">
 			<div className="container">
-			<h1 className="landingHeader">ELEMENTS</h1>
-			<Link to="/game" className="landingButton">START</Link>
-			<a href="https://docs.google.com/document/d/1HHIA1LgYNFDwX3jqK-7cGXw943NPmsdY-GeA4rEfUAA/edit?usp=sharing" target="_blank" className="rulesButton">RULES</a>
-		</div>
+				<h1 className="landingHeader">ELEMENTS</h1>
+				<Link to="/game" className="landingButton">START</Link>
+				<a
+					href="https://docs.google.com/document/d/1HHIA1LgYNFDwX3jqK-7cGXw943NPmsdY-GeA4rEfUAA/edit?usp=sharing"
+					className="rulesButton">RULES</a>
+			</div>
 		</div> )
 	}
 }
