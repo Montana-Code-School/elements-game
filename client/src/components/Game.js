1d5a37ca5b74457a5e6ffc60d1f97f4ce761dd44
Olesia <olesya.kovalchuk13@gmail.com>
2018-11-07 09:30:54 -0700

    shadow check fixed

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -177,12 +177,16 @@ class Game extends Component {
 		}
 	}
 	onCounterOffer = ( data ) => {
+		console.log( "inside function" )
 		if ( data.currentPlayer === this.state.playerName ) {
 			this.setState( { message: data.message } )
 		} else {
+			console.log( "counterOffer" )
 			if ( this.state.player.hand.water >= 1 && ( this.state.player.hand.earth >= 1 || this.state.player.hand.shadow >= 1 || this.state.player.hand.light >= 1 || this.state.player.hand.fire >= 1 ) ) {
+				console.log( "choice" )
 				this.modalContent( "choiceButton", "Would you like to counter?" )
 			} else if ( this.state.player.hand.water === 0 ) {
+				console.log( "no ability to counter" )
 				this.modalContent( "noWaterButton", "You are unable to counter at this time." )
 			}
 		}
@@ -253,7 +257,7 @@ class Game extends Component {
 			}, function () {
 				this.state.client.victoryCheck( this.state.room );
 				if ( this.state.afterFlip === "shadowAction" ) {
-					if ( this.state.opponent.hand === 0 ) {
+					if ( this.getCount( this.state.player.hand ) === 0 ) {
 						this.modalContent( "closeButton", "There are no elements in your hand to discard.", this.state.client.clickCard.bind( this, "none", this.state.room, this.state.afterFlip ) )
 					} else {
 						this.modalContent( "noButton" )
